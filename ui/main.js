edc9b4a8b4dbfa7e0fa4ab1b457657892ad7baba
cdrohith <rohithd007@gmail.com>
2016-10-02 12:12:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,17 @@ button.onclick = function() {
     request.open('GET','http://cdrohith.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+//Submit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function {
+    var names = ['name1','name2','name3'];
+    var list ='';
+    for(var i=0;i<names.length;i++)
+    {
+        list += '<li>' + names[i] + '</li>' ;
+    }
+    var ul = documnet.getElementById('namelist');
+    ul.innerHTML = list;
+};
